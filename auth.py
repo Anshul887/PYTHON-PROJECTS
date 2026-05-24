db36b489c8781c6c1fea2d62309ebe02f296c37b
Anshul887 <anshulmankar887@gmail.com>
2026-05-24 13:23:19 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,25 @@
+from database import execute_query, fetch_query
 
+def register(username, password):
+    user = fetch_query(
+        "SELECT * FROM users WHERE username=?",
+        (username,)
+    )
+
+    if user:
+        return False
+
+    execute_query(
+        "INSERT INTO users(username,password) VALUES(?,?)",
+        (username, password)
+    )
+
+    return True
+
+def login(username, password):
+    user = fetch_query(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username, password)
+    )
+
+    return bool(user)
