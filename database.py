6c404f32c8f0535424193a186cbaef190a43effd
Anshul887 <anshulmankar887@gmail.com>
2026-05-24 13:22:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,32 @@
+import sqlite3
 
+conn = sqlite3.connect("database/expenses.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS expenses(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    amount REAL,
+    category TEXT,
+    date TEXT
+)
+""")
+
+conn.commit()
+
+def execute_query(query, params=()):
+    conn.execute(query, params)
+    conn.commit()
+
+def fetch_query(query, params=()):
+    cursor = conn.execute(query, params)
+    return cursor.fetchall()
