ae3981bb12e67d5104910f17fccad894efadcf7f
Anshul887 <anshulmankar887@gmail.com>
2026-05-24 13:23:43 +0530

    Update analytics.py

--- analytics.py
+++ analytics.py
@@ -1,1 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from expense import get_expenses
 
+def generate_chart():
+    data = get_expenses()
+
+    if not data:
+        print("No expenses found")
+        return
+
+    df = pd.DataFrame(
+        data,
+        columns=["ID", "Title", "Amount", "Category", "Date"]
+    )
+
+    category_sum = df.groupby("Category")["Amount"].sum()
+
+    category_sum.plot(
+        kind="pie",
+        autopct="%1.1f%%",
+        figsize=(6,6)
+    )
+
+    plt.title("Expense Distribution")
+    plt.ylabel("")
+
+    plt.savefig("charts/expense_chart.png")
+    plt.show()
