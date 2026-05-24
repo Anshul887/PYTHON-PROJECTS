f243c814a69df293119a785c0cd7d298c60907bb
Anshul887 <anshulmankar887@gmail.com>
2026-05-24 13:22:36 +0530

    Update expence.py

--- expence.py
+++ expence.py
@@ -1,1 +1,10 @@
+from database import execute_query, fetch_query
 
+def add_expense(title, amount, category, date):
+    execute_query(
+        "INSERT INTO expenses(title,amount,category,date) VALUES(?,?,?,?)",
+        (title, amount, category, date)
+    )
+
+def get_expenses():
+    return fetch_query("SELECT * FROM expenses")
